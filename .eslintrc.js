5c1271b506e89584a384ab6c9244eb54eb281b86
Adam Lui <adamlui@pm.me>
2023-03-18 02:52:52 -0700

    Restored indent rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports  = {
   'rules': {
     'semi': ['error', 'always'], // enforce semicolons at end of statement
     'quotes': ['error', 'single'], // enforce single quotes for string literals
-/*    'indent': ['error', 4, { 'ignoredNodes': ['ConditionalExpression'], 'ignoreStrings': true }], // enforce 4-space indentation except for strings & conditionals */
+    'indent': ['error', 4, { 'ignoredNodes': ['ConditionalExpression'], 'ignoreStrings': true }], // enforce 4-space indentation except for strings & conditionals */
     'space-before-function-paren': ['error', 'never'], // enforce no space before function parentheses
     'brace-style': ['error', '1tbs', { 'allowSingleLine': true }], // enforce opening brace on same line as control statement, closing brace on own line
     'object-curly-spacing': ['error', 'always'], // enforce pacing in object literals
