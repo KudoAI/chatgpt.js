5b6785f54eeacf67fb87355641602cd51741c64c
Adam Lui 刘展鹏 <adam@chatgptjs.org>
2023-06-12 15:43:59 -0700

    Added semicolons

--- docs/js/for-the-badge.js
+++ docs/js/for-the-badge.js
@@ -1,9 +1,9 @@
 // Convert shield styles from `flat-square` to `for-the-badge`
 
 const observer = new MutationObserver(function() {
-	const shieldsDiv = document.querySelector('article#main > div:nth-child(2)')
+	const shieldsDiv = document.querySelector('article#main > div:nth-child(2)');
 	if (shieldsDiv) {
-		shieldsDiv.innerHTML = shieldsDiv.innerHTML.replace(/flat-square/g, 'for-the-badge')
-		observer.disconnect()
+		shieldsDiv.innerHTML = shieldsDiv.innerHTML.replace(/flat-square/g, 'for-the-badge');
+		observer.disconnect();
 	}
-}) ; observer.observe(document.body, { childList: true, subtree: true })
+}) ; observer.observe(document.body, { childList: true, subtree: true });
