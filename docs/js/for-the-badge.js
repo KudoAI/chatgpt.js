dba0b5171dee8816e127ed3b8ac3d67c752ccb6b
Adam Lui 刘展鹏 <adam@chatgptjs.org>
2023-06-12 15:39:40 -0700

    Removed unused `mutationsList` arg

--- docs/js/for-the-badge.js
+++ docs/js/for-the-badge.js
@@ -1,6 +1,6 @@
 // Convert shield styles from `flat-square` to `for-the-badge`
 
-const observer = new MutationObserver(function(mutationsList) {
+const observer = new MutationObserver(function() {
 	const shieldsDiv = document.querySelector('article#main > div:nth-child(2)')
 	if (shieldsDiv) {
 		shieldsDiv.innerHTML = shieldsDiv.innerHTML.replace(/flat-square/g, 'for-the-badge')
