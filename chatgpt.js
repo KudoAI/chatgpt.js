f09e07fed01a1e2b15dea188307a55356954c83f
Adam Lui 刘展鹏 <adam@chatgptjs.org>
2023-06-26 01:19:06 -0700

    Changed `chatgpt.sidebar.isOff()` to boolean

--- chatgpt.js
+++ chatgpt.js
@@ -660,7 +660,7 @@ var chatgpt = {
 
     sidebar: {
         isOn: function() { return !document.querySelector('button[aria-label*="Show sidebar"]'); },
-        isOff: function() { return document.querySelector('button[aria-label*="Show sidebar"]'); },
+        isOff: function() { return !!document.querySelector('button[aria-label*="Show sidebar"]'); },
         hide: function() { this.isOn() ? this.toggle() : console.info( '🤖 chatgpt.js >> Sidebar already hidden!'); },
         show: function() { this.isOff() ? this.toggle() : console.info( '🤖 chatgpt.js >> Sidebar already shown!'); },
 
