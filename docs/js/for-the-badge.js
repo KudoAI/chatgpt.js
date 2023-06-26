f0831e09ada40af087e493ad16aea6da0ca7ed4c
adamlui@protonmail.com <adamlui@protonmail.com>
2023-06-25 17:51:41 -0700

    Added tiles modifier, renamed to `shields-tiles-modifier.js`

--- docs/js/for-the-badge.js
+++
@@ -1,9 +0,0 @@
-// Convert shield styles from `flat-square` to `for-the-badge`
-
-const observer = new MutationObserver(function() {
-	const shieldsDiv = document.querySelector('article#main > div:nth-child(2)');
-	if (shieldsDiv) {
-		shieldsDiv.innerHTML = shieldsDiv.innerHTML.replace(/flat-square/g, 'for-the-badge');
-		observer.disconnect();
-	}
-}) ; observer.observe(document.body, { childList: true, subtree: true });
---
+++ docs/js/shields-tiles-modifier.js
@@ -0,0 +1,21 @@
+// Modify shield + showcase promo tile attributes
+
+const onLoadObserver = new MutationObserver(() => {
+
+    // Modify shields
+    const shields = document.querySelectorAll('article#main > div:nth-child(2) img')
+    if (shields) { // change styles to for-the-badge
+        shields.forEach((img) => {
+            img.setAttribute('src', img.getAttribute('src')
+            	.replace(/(style="[^"]*")/g, 'style="for-the-badge"')
+    )})}
+
+	// Modify showcase promo tiles
+    const promoTiles = document.querySelectorAll('img[src*="promo-tile"]')
+    if (promoTiles.length > 0) // change widths to 1111
+    	promoTiles.forEach((img) => { img.setAttribute('width', '1111') })
+
+    // Disconnect observer
+	if (shields.length > 0 || promoTiles.length > 0) onLoadObserver.disconnect()
+
+}) ; onLoadObserver.observe(document.body, { childList: true, subtree: true })
