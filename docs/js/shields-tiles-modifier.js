c2d3c4ab25ff1ba656cb6d78384b778b74a044d5
adamlui@protonmail.com <adamlui@protonmail.com>
2023-06-25 17:58:15 -0700

    Replaced tabs w/ spaces, added semicolons

--- docs/js/shields-tiles-modifier.js
+++ docs/js/shields-tiles-modifier.js
@@ -3,19 +3,19 @@
 const onLoadObserver = new MutationObserver(() => {
 
     // Modify shields
-    const shields = document.querySelectorAll('article#main > div:nth-child(2) img')
+    const shields = document.querySelectorAll('article#main > div:nth-child(2) img');
     if (shields) { // change styles to for-the-badge
         shields.forEach((img) => {
             img.setAttribute('src', img.getAttribute('src')
-            	.replace(/(style="[^"]*")/g, 'style="for-the-badge"')
-    )})}
+                .replace(/(style="[^"]*")/g, 'style="for-the-badge"')
+    );});}
 
 	// Modify showcase promo tiles
-    const promoTiles = document.querySelectorAll('img[src*="promo-tile"]')
+    const promoTiles = document.querySelectorAll('img[src*="promo-tile"]');
     if (promoTiles.length > 0) // change widths to 1111
-    	promoTiles.forEach((img) => { img.setAttribute('width', '1111') })
+        promoTiles.forEach((img) => { img.setAttribute('width', '1111'); });
 
     // Disconnect observer
-	if (shields.length > 0 || promoTiles.length > 0) onLoadObserver.disconnect()
+    if (shields.length > 0 || promoTiles.length > 0) onLoadObserver.disconnect();
 
-}) ; onLoadObserver.observe(document.body, { childList: true, subtree: true })
+}) ; onLoadObserver.observe(document.body, { childList: true, subtree: true });
