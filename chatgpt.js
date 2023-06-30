bbf5cb218ec95345809015bf9b26942fdcb8f805
Adam Lui 刘展鹏 <adam@chatgptjs.org>
2023-06-30 08:27:15 -0700

    Added `chatgpt.uuidv4()`

--- chatgpt.js
+++ chatgpt.js
@@ -727,6 +727,17 @@ var chatgpt = {
         const [schemeToRemove, schemeToAdd] = document.documentElement.classList.contains('dark') ? ['dark', 'light'] : ['light', 'dark'];
         document.documentElement.classList.replace(schemeToRemove, schemeToAdd);
         document.documentElement.style.colorScheme = schemeToAdd;
+    },
+
+    function uuidv4() {
+        let d = new Date().getTime() // get current timestamp in ms (to ensure UUID uniqueness)
+        const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+            const r = ( // generate random nibble
+                (d + (window.crypto.getRandomValues(new Uint32Array(1))[0] / (Math.pow(2, 32) - 1))*16)%16 | 0 )
+            d = Math.floor(d/16) // correspond each UUID digit to unique 4-bit chunks of timestamp
+            return ( c == 'x' ? r : (r&0x3|0x8) ).toString(16) // generate random hexadecimal digit
+        })
+        return uuid
     }
 };
 
