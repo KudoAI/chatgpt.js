d498d15347dfc9758d00011847ae6057976fb920
Adam Lui 刘展鹏 <adam@kudoai.com>
2023-07-07 07:41:29 -0700

    Updated icon meta URLs

--- starters/greasemonkey/chatgpt.js-greasemonkey-starter.user.js
+++ starters/greasemonkey/chatgpt.js-greasemonkey-starter.user.js
@@ -3,11 +3,11 @@
 // @description   A Greasemonkey template to start using chatgpt.js
 // @author        chatgpt.js
 // @namespace     https://github.com/kudoai/chatgpt.js
-// @version       2023.7.7.5
+// @version       2023.7.7.6
 // @license       MIT
 // @match         https://chat.openai.com/*
-// @icon          https://raw.githubusercontent.com/kudoai/chatgpt.js-greasemonkey-starter/main/media/icons/robot/icon48.png
-// @icon64        https://raw.githubusercontent.com/kudoai/chatgpt.js-greasemonkey-starter/main/media/icons/robot/icon64.png
+// @icon          https://raw.githubusercontent.com/kudoai/chatgpt.js-greasemonkey-starter/main/media/images/icons/robot/icon48.png
+// @icon64        https://raw.githubusercontent.com/kudoai/chatgpt.js-greasemonkey-starter/main/media/images/icons/robot/icon64.png
 // @require       https://cdn.jsdelivr.net/gh/kudoai/chatgpt.js@8483b553675c3444db5c6b40a8686531c11b2a35/dist/chatgpt-1.11.0.min.js
 // @grant         none
 // @noframes
