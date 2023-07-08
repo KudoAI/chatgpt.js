7ed1f41e224c6424fab60080607603c4b119c731
adamlui@protonmail.com <adamlui@protonmail.com>
2023-07-08 09:54:52 -0700

    Added semicolon

--- starters/chrome/extension/background.js
+++ starters/chrome/extension/background.js
@@ -1,4 +1,4 @@
 // Open ChatGPT on install
 chrome.runtime.onInstalled.addListener((details) => {
     if (details.reason == 'install') chrome.tabs.create({ url: 'https://chat.openai.com/' });
-})
+});
