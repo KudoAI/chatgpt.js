f0cdfc98a4e1f805b611c0c4ace09b1cb46ca634
adamlui@protonmail.com <adamlui@protonmail.com>
2023-07-08 09:47:08 -0700

    Added Chrome starter

---
+++ starters/chrome/docs/README.md
@@ -0,0 +1,7 @@
+# 🙈 chatgpt.js-chrome-starter
+
+A starting point for developing your own Chrome extension using [chatgpt.js](https://github.com/kudoai/chatgpt.js)
+
+<br>
+
+![](https://raw.githubusercontent.com/kudoai/chatgpt.js/main/starters/chrome/media/images/screenshots/extension-loaded.png)
---
+++ starters/chrome/extension/content.js
@@ -0,0 +1,18 @@
+// NOTE: This script relies on the powerful chatgpt.js library @ https://chatgpt.js.org
+// (c) 2023 KudoAI & contributors under the MIT license
+// Source: https://github.com/kudoai/chatgpt.js
+
+(async () => {
+
+    // Import libs
+    const { chatgpt } = await import(chrome.runtime.getURL('lib/chatgpt.js'));
+
+    await chatgpt.isLoaded();
+    chatgpt.printAllFunctions(); // to console
+    chatgpt.alert('≫ ChatGPT extension loaded! 🚀', // title
+        'Success! Press Ctrl+Shift+I to view all chatgpt.js methods.', // msg
+        function getHelp() { window.open(GM_info.scriptSupportURL || GM_info.script.supportURL || ''); }); // button
+
+    // Your code here...
+
+})()
