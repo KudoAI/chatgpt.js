ae3a89b75b2d343d407f25e10c261da9eea862b3
Adam Lui 刘展鹏 <adam@kudoai.com>
2023-07-07 18:22:12 -0700

    Updated description/namespace/homepage metas

--- starters/greasemonkey/chatgpt.js-greasemonkey-starter.user.js
+++ starters/greasemonkey/chatgpt.js-greasemonkey-starter.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name          ChatGPT Userscript
-// @description   A Greasemonkey template to start using chatgpt.js
+// @description   A Greasemonkey template to start using chatgpt.js like a boss
 // @author        chatgpt.js
-// @namespace     https://github.com/kudoai/chatgpt.js
-// @version       2023.7.7.6
+// @namespace     https://chatgpt.js.org
+// @version       2023.7.7.7
 // @license       MIT
 // @match         https://chat.openai.com/*
 // @icon          https://raw.githubusercontent.com/kudoai/chatgpt.js-greasemonkey-starter/main/media/images/icons/robot/icon48.png
@@ -11,7 +11,7 @@
 // @require       https://cdn.jsdelivr.net/gh/kudoai/chatgpt.js@8483b553675c3444db5c6b40a8686531c11b2a35/dist/chatgpt-1.11.0.min.js
 // @grant         none
 // @noframes
-// @homepageURL   https://chatgpt.js.org
+// @homepageURL   https://github.com/kudoai/chatgpt.js-greasemonkey-starter
 // @supportURL    https://github.com/kudoai/chatgpt.js-greasemonkey-starter/issues
 // ==/UserScript==
 
