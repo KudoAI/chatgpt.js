89e74b87e7980a8334fe8a39b9da6ed01fa9ab81
adamlui@protonmail.com <adamlui@protonmail.com>
2023-07-09 07:04:51 -0700

    Added element hiding + link manipulation

--- docs/assets/js/onload-modifier.js
+++ docs/assets/js/onload-modifier.js
@@ -1,19 +1,44 @@
-// Insert footer
+// Hack page elements on load
+
+const mdLoaded = new Promise((resolve) => {
+    const mdObserver = new MutationObserver((mutationsList, observer) => {
+        if (document.querySelector('article div')) { observer.disconnect(); resolve(); }});
+    mdObserver.observe(document.body, { childList: true, subtree: true });
+});
 
 const onLoadObserver = new MutationObserver(() => {
 
     // Exit if not loaded
-    const shields = document.querySelectorAll('article#main > div:nth-child(2) img');
-    if (shields.length === 0) return;
+    if (!document.querySelector('.github-corner')) return;
 
-    // Append footer
-    const footer = document.createElement('div');
-    fetch('assets/html/footer.html')
-        .then(response => response.text()).then(html => {
-            footer.innerHTML = html;
-            const article = document.querySelector('article');
-            article.insertBefore(footer, article.lastElementChild);
+    // Hide readme cover + append footer to HOMEPAGE
+    if (location.hash.match(/#\/(\w{2}(-\w{2})?\/)?$/)) {
+        document.querySelector('article > div:first-of-type').style.display = 'none';
+        const footer = document.createElement('div');
+        fetch('assets/html/footer.html')
+            .then(response => response.text()).then(html => {
+                footer.innerHTML = html;
+                const article = document.querySelector('article');
+                article.insertBefore(footer, article.lastElementChild);
+            });
+
+    // Hide site lang selector from NON-HOME pages
+    } else document.querySelector('.app-nav').style.display = 'none';
+
+    // Hack LICENSE/SECURIY pages
+    if (location.hash.match(/LICENSE|SECURITY/)) {
+
+        // Hide sidebar + toggle
+        document.body.className = 'ready close';
+        document.querySelector('.sidebar-toggle-button').style.display = 'none';
+
+        // Correct doc lang selector links
+        mdLoaded.then(() => {
+            const docLangSelector = document.querySelectorAll('h5 a');
+            for (const lang of docLangSelector)
+                lang.href = lang.href.replace(/(.*\/\/.*?\/)((\w{2}(-\w{2})?\/)?.*)\.md/, '$1#/$2');
         });
+    }
 
     // Disconnect observer
     onLoadObserver.disconnect();
@@ -21,3 +46,7 @@ const onLoadObserver = new MutationObserver(() => {
 });
 
 onLoadObserver.observe(document.body, { childList: true, subtree: true });
+
+// Re-connect observre on nav to new hash
+window.addEventListener('hashchange', () => { 
+    onLoadObserver.observe(document.body, { childList: true, subtree: true }); })
