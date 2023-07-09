042fcb7347251334ccaf6502dacedfb562e8c815
Adam Lui 刘展鹏 <adam@kudoai.com>
2023-07-08 22:22:38 -0700

    Added copyright notice

--- starters/greasemonkey/chatgpt.js-greasemonkey-starter.user.js
+++ starters/greasemonkey/chatgpt.js-greasemonkey-starter.user.js
@@ -3,7 +3,7 @@
 // @description   A Greasemonkey template to start using chatgpt.js like a boss
 // @author        chatgpt.js
 // @namespace     https://chatgpt.js.org
-// @version       2023.7.8.2
+// @version       2023.7.8.3
 // @license       MIT
 // @match         https://chat.openai.com/*
 // @icon          https://raw.githubusercontent.com/kudoai/chatgpt.js-greasemonkey-starter/main/media/images/icons/robot/icon48.png
@@ -14,6 +14,8 @@
 // @homepageURL   https://github.com/kudoai/chatgpt.js-greasemonkey-starter
 // @supportURL    https://github.com/kudoai/chatgpt.js-greasemonkey-starter/issues
 // ==/UserScript==
+
+// NOTE: This script relies on the powerful chatgpt.js library @ https://chatgpt.js.org (c) 2023 KudoAI & contributors under the MIT license.
 
 (async () => {
     await chatgpt.isLoaded();
