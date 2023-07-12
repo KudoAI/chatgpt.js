b86b540717e8b8edd937d0ca07915edb60b49ff7
adamlui@protonmail.com <adamlui@protonmail.com>
2023-07-12 07:35:58 -0700

    Added auto-enable + extension state/settings sync

--- starters/chrome/extension/background.js
+++ starters/chrome/extension/background.js
@@ -1,4 +1,12 @@
-// Open ChatGPT on install
+// Add install/update actions
 chrome.runtime.onInstalled.addListener((details) => {
-    if (details.reason == 'install') chrome.tabs.create({ url: 'https://chat.openai.com/' });
+    chrome.storage.local.set({ 'chatgptJS_extensionDisabled': false }); // auto-enable
+    if (details.reason == 'install') chrome.tabs.create({ url: 'https://chat.openai.com/' }); // open ChatGPT
 });
+
+// Sync extension state/settings when ChatGPT tab active
+chrome.tabs.onActivated.addListener((activeInfo) => {
+    chrome.tabs.get(activeInfo.tabId, (tab) => {
+        if (tab.url.match(/^https:\/\/chat\.openai\.com/)) {
+            chrome.tabs.sendMessage(tab.id, { action: 'syncExtension' });
+}});});
