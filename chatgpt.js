d1231cad01a576396554dba2e529a7e2d38b4a52
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2023-07-15 16:46:54 +0200

    added debug logging

--- chatgpt.js
+++ chatgpt.js
@@ -924,30 +924,14 @@ const chatgpt = {
         }} setTimeout(() => { chatgpt.send(msg); }, 500);
     },
 
-    shareChat: function() {
-        const validDetails = [ 'is_anonymous', 'is_public', 'is_visible', 'share_id', 'share_url', 'title' ];
-        let chatToGet = 1, anonymous = true, detailsToGet = [];
-        if (validDetails.includes(arguments[0])) // if 1st arg is detail string
-            detailsToGet = Array.from(arguments); // convert to array
-        else { // handle chat passed/unpassed + anonymous passes/unpassed + details as array/arg(s)/unpassed
-            const chatPassed = Array.isArray(arguments[0]) || !arguments[0] ? false : true;
-            const anonPassed = Array.isArray(arguments[1]) || !arguments[1] ? false : true;
-            chatToGet = chatPassed ? arguments[0] : 0;
-            anonymous = anonPassed ? arguments[1] : true;
-            const detailsIdx = arguments[0] === '' ? 2 : +chatPassed+anonPassed; // offset detailsToGet index from chatToGet and anonymous
-            detailsToGet = ( !arguments[detailsIdx] ? validDetails // no details passed, populate w/ all valid ones
-                    : Array.isArray(arguments[detailsIdx]) ? arguments[detailsIdx] // details array passed, do nothing
-                    : Array.from(arguments).slice(detailsIdx) ); // details string(s) passed, convert to array
-        }
-
+    shareChat: function(chatToGet) {
         return new Promise((resolve) => {
             chatgpt.getAccessToken().then(token => { // get access token
                 getChatNode(token).then(node => { // get chat node
-                    initShare(token, node).then(data => { // initialize share
-                        confirmShareChat(token, data).then(() => { // confirm share
-                            const detailsToReturn = {};
-                            for (const detail of detailsToGet) detailsToReturn[detail] = data[detail];
-                            return resolve(detailsToReturn); // resolve main Promise returning the requested data
+                    makeChatToShare(token, node).then(data => {
+                        confirmShareChat(token, data).then(() => {
+                            resolve();
+                            // window.open(data.share_url, '_blank');
                         });
                     });
                 });
@@ -968,7 +952,7 @@ const chatgpt = {
                     xhr.send();
         });});}
 
-        function initShare(token, node) {
+        function makeChatToShare(token, node) {
             return new Promise((resolve, reject) => {
                 const xhr = new XMLHttpRequest();
                 chatgpt.getChatDetails(chatToGet).then(chat => {
@@ -977,13 +961,14 @@ const chatgpt = {
                     xhr.setRequestHeader('Authorization', 'Bearer ' + token);
                     xhr.onload = () => {
                         if (xhr.status !== 200) return reject('🤖 chatgpt.js >> Request failed. Cannot initialize share chat.');
-                        return resolve(JSON.parse(xhr.responseText)); // return untouched data needed for confirmShareChat
+                        console.log(JSON.parse(xhr.responseText)); // DEBUG
+                        return resolve(JSON.parse(xhr.responseText)); // return untouched data
                     };
                     xhr.send(JSON.stringify( // request body
                         {
                             current_node_id: node, // by getChatNode
                             conversation_id: chat.id, // current chat id
-                            is_anonymous: anonymous // show user name in the conversation or not
+                            is_anonymous: true // show user name in the conversation or not
                         }
                     ));
                 });
@@ -997,16 +982,17 @@ const chatgpt = {
                 xhr.setRequestHeader('Authorization', 'Bearer ' + token);
                 xhr.onload = () => {
                     if (xhr.status !== 200) return reject('🤖 chatgpt.js >> Request failed. Cannot share chat.');
-                    return resolve(); // the response has nothing useful, so return nothing
+                    console.log(JSON.parse(xhr.responseText)); // DEBUG
+                    return resolve(); // the response has nothing useful
                 };
                 xhr.send(JSON.stringify( // request body
-                    { // parse data from initShare
+                    {
                         share_id: data.share_id,
                         highlighted_message_id: data.highlighted_message_id,
                         title: data.title,
                         is_public: data.is_public,
                         is_visible: data.is_visible,
-                        is_anonymous: anonymous
+                        is_anonymous: data.is_anonymous
                     }
                 ));
             });
