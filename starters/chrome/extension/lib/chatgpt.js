1d6f44eea1fb635b6ff27e54992ec50e535d63c0
adamlui <adamlui@users.noreply.github.com>
2023-07-17 05:11:29 +0000

    Revert `reAttributes` optimizations
    ...to reduce recursion ↞ [auto-sync from `kudoai/chatgpt.js`]

--- starters/chrome/extension/lib/chatgpt.js
+++ starters/chrome/extension/lib/chatgpt.js
@@ -786,7 +786,7 @@ const chatgpt = {
 
     renderHTML: function(node) {
         const reTags = /<([a-z\d]+)\b([^>]*)>([\s\S]*?)<\/\1>/g;
-        const reAttributes = /(\S+)=['"]?((?:[^"'\s]+|"(?:\\.|[^"])*"|'(?:\\.|[^'])*'|style\s*=\s*['"](?!color:)\s*[^'"]+)*)(?=["']|$)/g;
+        const reAttributes = /(\S+)=['"]?((?:.(?!['"]?\s+(?:\S+)=|[>']))+.)['"]?/g;
         const nodeContent = node.childNodes;
 
         // Preserve consecutive spaces + line breaks
