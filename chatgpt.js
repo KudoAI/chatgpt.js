8f7a54f21ff867ce7cbb3f362674638dfb4fa027
Adam Lui 刘展鹏 <adam@kudoai.com>
2023-07-17 01:13:39 -0700

    Replaced `for` loop in `chatgpt.notify()` w/ `for-of` one...
    - Removed commented out code

--- chatgpt.js
+++ chatgpt.js
@@ -711,10 +711,9 @@ const chatgpt = {
         if (thisQuadrantDivIDs.length > 1) {
             try { // to move old notifications
                 const divsToMove = thisQuadrantDivIDs.slice(0, -1); // exclude new div
-                for (let j = 0; j < divsToMove.length; j++) {
-                    const oldDiv = document.getElementById(divsToMove[j]);
+                for (const divId of thisQuadrantDivIDs.slice(0, -1)) {
+                    const oldDiv = document.getElementById(divId);
                     const offsetProp = oldDiv.style.top ? 'top' : 'bottom'; // pick property to change
-                    // const vOffset = +oldDiv.style[offsetProp].match(/\d+/)[0] + 5 + oldDiv.getBoundingClientRect().height;
                     const vOffset = +/\d+/.exec(oldDiv.style[offsetProp])[0] + 5 + oldDiv.getBoundingClientRect().height;
                     oldDiv.style[offsetProp] = `${vOffset}px`; // change prop
                 }
