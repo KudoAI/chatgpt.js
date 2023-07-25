ac3b5c94f8d256b7d6e69c825d39972fb0e30c12
adamlui@protonmail.com <adamlui@protonmail.com>
2023-07-25 13:47:23 -0700

    Converted double-quotes to single ones

--- docs/assets/js/back-to-top.js
+++ docs/assets/js/back-to-top.js
@@ -5,14 +5,14 @@ const button = document.createElement('button');
 button.classList.add('back-to-top');
 
 // Create button SVG
-const svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-svgElement.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-svgElement.setAttribute("height", "2em");
-svgElement.setAttribute("viewBox", "0 0 448 512");
+const svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+svgElement.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+svgElement.setAttribute('height', '2em');
+svgElement.setAttribute('viewBox', '0 0 448 512');
 
 // Create button SVG path
-const pathElement = document.createElementNS("http://www.w3.org/2000/svg", "path");
-pathElement.setAttribute("d", "M246.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 109.3 361.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160zm160 352l-160-160c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 301.3 361.4 438.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3z");
+const pathElement = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+pathElement.setAttribute('d', 'M246.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 109.3 361.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160zm160 352l-160-160c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 301.3 361.4 438.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3z');
 
 // Merge elements
 svgElement.appendChild(pathElement); button.appendChild(svgElement);
