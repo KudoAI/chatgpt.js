4996fef2c7fc6cd632f57d9696ac0d745790be32
adamlui@protonmail.com <adamlui@protonmail.com>
2023-07-25 19:29:46 -0700

    Added animated feature list

--- docs/assets/js/onload-modifier.js
+++ docs/assets/js/onload-modifier.js
@@ -6,6 +6,13 @@ const mdLoaded = new Promise((resolve) => {
     mdObserver.observe(document.body, { childList: true, subtree: true });
 });
 
+let featureListInView = false;            
+featureListObserver = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting) {
+        featureListInView = true;
+        featureListObserver.unobserve(document.querySelector('#feature-list')); 
+}});
+
 const onLoadObserver = new MutationObserver(() => {
 
     // Exit if not loaded
@@ -26,7 +33,49 @@ const onLoadObserver = new MutationObserver(() => {
                 document.getElementById('dropdown-button').innerText = activeLanguage;
             }, 15);
 
-            // Convert showcase OpenAI icons to dark-mode
+            // Create/select FEATURE LIST
+            const featureListDiv = document.querySelector('#feature-list') || // select div
+                                   document.createElement('div'); // ...or create it
+            if (!featureListDiv.parentElement) { // append created div if not in DOM
+                featureListDiv.setAttribute('id', 'feature-list');
+                const aboutH2 = document.querySelector('h2#about');
+                aboutH2.parentElement.insertBefore( // insert after description
+                    featureListDiv, aboutH2.nextSibling.nextSibling);
+            }
+
+            // ...then observe for when it's in view (self-disconnects)
+            if (!featureListInView) featureListObserver.observe(featureListDiv); 
+
+            // ...then loop check for observer flag to begin typing
+            const txtToType = new Array( // features to type
+                '>>  Feature-rich', '>>  Object-oriented', '>>  Easy-to-use',
+                '>>  Lightweight (yet optimally performant)');
+            const typeSpeed = 30; // ms between chars typed
+            let iniTxtToType = 0; // index of array txt to start typing
+            let iniTxtPos = 3; // position in txt to start typing from
+            const linesToScrollAt = 5; // start scrolling up at this many lines
+            let iniArrLength = txtToType[0].length; // initial length of txt array
+            let typeContent = ''; // init contents variable
+            let iniRow; // init current row     
+            function typeText() {
+                typeContent =  ' ';
+                iniRow = Math.max(0, iniTxtToType - linesToScrollAt);
+                const destination = document.getElementById('feature-list');
+                while (iniRow < iniTxtToType) typeContent += txtToType[iniRow++] + '<br /><br />';
+                destination.innerHTML = typeContent + txtToType[iniTxtToType].substring(0, iniTxtPos) + '_';
+                if (iniTxtPos++ == iniArrLength) {
+                    iniTxtPos = 0; iniTxtToType++;
+                    if (iniTxtToType != txtToType.length) { // if end of string reached
+                        iniArrLength = txtToType[iniTxtToType].length; // reset array length
+                        setTimeout(typeText, 88); // pause til next string
+                }} else setTimeout(typeText, typeSpeed);
+            }
+            (function checkOrTypeFeatureList() {
+                if (featureListInView) typeText();
+                else setTimeout(checkOrTypeFeatureList, 100); 
+            })();
+
+            // Convert OpenAI icons in showcase apps to dark-mode
             document.querySelectorAll('picture').forEach(picture => {
                 const srcElement = picture.querySelector('source');
                 const srcSet = srcElement.getAttribute('srcset');
