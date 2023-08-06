4ff6d18b22ff8fcedf02df73b608a3ade11617d1
adamlui@protonmail.com <adamlui@protonmail.com>
2023-08-06 06:04:22 -0700

    Exposed `velocity` globally (for onload-hacks.js)

--- docs/assets/js/src/starry-background.js
+++ docs/assets/js/src/starry-background.js
@@ -3,14 +3,14 @@
  *  .  .      .      *     .      .   *     */
 
 // Init variables
-window.starColor = '#fff';
+window.starColor = 'white';
+window.starVelocity = { x: 0, y: 0, tx: 0, ty: 0, z: 0.0005 };
 const starSize = 6, starMinScale = 0.00000000025, overflowThreshold = 50,
       starCount = ( window.innerWidth + window.innerHeight ) / 18,
       canvas = document.querySelector( 'canvas' ),
       context = canvas.getContext( '2d' );
 let scale = 0.5, // device pixel ratio
     stars = [], width, height, pointerX, pointerY,
-    velocity = { x: 0, y: 0, tx: 0, ty: 0, z: 0.0005 },
     touchInput = false;
 
 // Generate/animate stars
@@ -46,26 +46,26 @@ function animateStars() {
     context.clearRect(0, 0, width, height);
 
     // Update star positions
-    velocity.tx *= 0.86; velocity.ty *= 0.86; // proportional to momentum
-    velocity.x += ( velocity.tx - velocity.x ) * 0.8;
-    velocity.y += ( velocity.ty - velocity.y ) * 0.8;
+    window.starVelocity.tx *= 0.86; window.starVelocity.ty *= 0.86; // proportional to momentum
+    window.starVelocity.x += ( window.starVelocity.tx - window.starVelocity.x ) * 0.8;
+    window.starVelocity.y += ( window.starVelocity.ty - window.starVelocity.y ) * 0.8;
     stars.forEach((star) => {
-        star.x += velocity.x * star.z;
-        star.y += velocity.y * star.z;
-        star.x += (star.x - width/2) * velocity.z * star.z;
-        star.y += (star.y - height/2) * velocity.z * star.z;
-        star.z += velocity.z;
+        star.x += window.starVelocity.x * star.z;
+        star.y += window.starVelocity.y * star.z;
+        star.x += (star.x - width/2) * window.starVelocity.z * star.z;
+        star.y += (star.y - height/2) * window.starVelocity.z * star.z;
+        star.z += window.starVelocity.z;
   
         // Recycle star when out-of-bounds
         if (star.x < -overflowThreshold || star.x > width + overflowThreshold ||
                 star.y < -overflowThreshold || star.y > height + overflowThreshold) {            
-            let direction = 'z', vx = Math.abs(velocity.x), vy = Math.abs(velocity.y);
+            let direction = 'z', vx = Math.abs(window.starVelocity.x), vy = Math.abs(window.starVelocity.y);
             if (vx > 1 || vy > 1) {
                 let axis;
                 if (vx > vy) axis = Math.random() < vx / ( vx + vy ) ? 'h' : 'v';
                 else axis = Math.random() < vy / ( vx + vy ) ? 'v' : 'h';
-                if (axis === 'h') direction = velocity.x > 0 ? 'l' : 'r';
-                else direction = velocity.y > 0 ? 't' : 'b';
+                if (axis === 'h') direction = window.starVelocity.x > 0 ? 'l' : 'r';
+                else direction = window.starVelocity.y > 0 ? 't' : 'b';
             }
             star.z = starMinScale + Math.random() * ( 1 - starMinScale );
             if (direction === 'z') { star.z = 0.1; star.x = Math.random() * width; star.y = Math.random() * height; }
@@ -85,8 +85,8 @@ function animateStars() {
         context.strokeStyle = window.starColor;
         context.beginPath();
         context.moveTo( star.x, star.y );
-        let tailX = velocity.x * 2,
-            tailY = velocity.y * 2;
+        let tailX = window.starVelocity.x * 2,
+            tailY = window.starVelocity.y * 2;
 
         // stroke() wont work on an invisible line
         if (Math.abs(tailX) < 0.1) tailX = 0.5;
@@ -103,8 +103,8 @@ function animateStars() {
 function movePointer(x, y) {
     if (typeof pointerX === 'number' && typeof pointerY === 'number') {
         let ox = x - pointerX, oy = y - pointerY;
-        velocity.tx = velocity.tx + ( ox / 8*scale ) * ( touchInput ? 1 : -1 );
-        velocity.ty = velocity.ty + ( oy / 8*scale ) * ( touchInput ? 1 : -1 );
+        window.starVelocity.tx = window.starVelocity.tx + ( ox / 8*scale ) * ( touchInput ? 1 : -1 );
+        window.starVelocity.ty = window.starVelocity.ty + ( oy / 8*scale ) * ( touchInput ? 1 : -1 );
     }
     pointerX = x; pointerY = y;
 }
