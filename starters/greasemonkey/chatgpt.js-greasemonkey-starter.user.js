364f376112a677159c1fa12d166a1e0385b4661b
Adam Lui <adam@kudoai.com>
2023-08-25 06:58:46 -0700

    Added perma-dismiss checkbox to alert + config r/w

--- starters/greasemonkey/chatgpt.js-greasemonkey-starter.user.js
+++ starters/greasemonkey/chatgpt.js-greasemonkey-starter.user.js
@@ -3,13 +3,14 @@
 // @description   A Greasemonkey template to start using chatgpt.js like a boss
 // @author        chatgpt.js
 // @namespace     https://chatgpt.js.org
-// @version       2023.8.24
+// @version       2023.8.25
 // @license       MIT
 // @match         https://chat.openai.com/*
 // @icon          https://raw.githubusercontent.com/kudoai/chatgpt.js-greasemonkey-starter/main/media/images/icons/robot/icon48.png
 // @icon64        https://raw.githubusercontent.com/kudoai/chatgpt.js-greasemonkey-starter/main/media/images/icons/robot/icon64.png
 // @require       https://cdn.jsdelivr.net/gh/kudoai/chatgpt.js@a73c962d8ea7a48d81e8fd260c91b23175753b59/dist/chatgpt-2.1.1.min.js
-// @grant         none
+// @grant         GM_getValue
+// @grant         GM_setValue
 // @noframes
 // @homepageURL   https://github.com/kudoai/chatgpt.js-greasemonkey-starter
 // @supportURL    https://github.com/kudoai/chatgpt.js-greasemonkey-starter/issues
@@ -18,14 +19,34 @@
 // NOTE: This script relies on the powerful chatgpt.js library @ https://chatgpt.js.org (c) 2023 KudoAI & contributors under the MIT license
 
 (async () => {
-    await chatgpt.isLoaded();
-    chatgpt.printAllFunctions(); // in console
-    chatgpt.alert('≫ ChatGPT script loaded! 🚀', // title
-        'Success! Press Ctrl+Shift+'
-            + ( navigator.userAgent.indexOf('Firefox') > -1 ? 'K' : 'J' )
-            + ' to view all chatgpt.js functions.', // msg
-        function getHelp() { window.open('https://github.kudoai.com/chatgpt.js-greasemonkey-starter/issues', '_blank', 'noopener'); }); // button
+
+    // Init config
+    const config = { prefix: 'chatgptScript' }; loadSetting('skipAlert');
+
+    // Print chatgpt.js methods
+    await chatgpt.isLoaded(); chatgpt.printAllFunctions(); // to console
+
+    // Show alert
+    if (!config.skipAlert) {
+        chatgpt.alert('≫ ChatGPT script loaded! 🚀', // title
+            'Success! Press Ctrl+Shift+' // msg
+                + ( navigator.userAgent.indexOf('Firefox') > -1 ? 'K' : 'J' )
+                + ' to view all chatgpt.js functions.',
+            function getHelp() { // button
+                window.open('https://github.kudoai.com/chatgpt.js-greasemonkey-starter/issues', '_blank', 'noopener'); },
+            function dontShowAgain() { // checkbox
+                saveSetting('skipAlert', !config.skipAlert); });
+    }
 
     // Your code here...
+    // Your code here...
+    // Your code here...
+    // Your code here...
+    // Your code here...
+    // Your code here...
+
+    // Define HELPER functions
+    function loadSetting(...keys) { keys.forEach(key => { config[key] = GM_getValue(config.prefix + '_' + key, false); });}
+    function saveSetting(key, value) { GM_setValue(config.prefix + '_' + key, value); config[key] = value; }
 
 })();
