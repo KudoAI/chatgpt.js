6cb85bcf0b5277dbd9dd8dab7e5ebbcceb8f2c1b
adamlui <adamlui@users.noreply.github.com>
2023-08-29 01:44:04 +0000

    Re-insert alias function generation routine ↞ [auto-sync from `kudoai/chatgpt.js`]

--- starters/chrome/extension/lib/chatgpt.js
+++ starters/chrome/extension/lib/chatgpt.js
@@ -1473,6 +1473,43 @@ for (const buttonAction of buttonActions) {
     };
 }
 
+// Create alias functions
+const camelCaser = (words) => {
+    return words.map((word, index) => index === 0 || word === 's' ? word : word.charAt(0).toUpperCase() + word.slice(1)).join(''); };
+for (const prop in chatgpt) {
+
+    // Create new function for each alias
+    for (const subAliases of functionAliases) {
+        if (subAliases.includes(prop)) {
+            if (subAliases.some(element => element.includes('.'))) {
+                const nestedFunction = subAliases.find(element => element.includes('.')).split('.')[1];
+                for (const nestAlias of subAliases) {
+                    if (/^(\w+)/.exec(nestAlias)[1] !== prop) { // don't alias og function
+                        chatgpt[nestAlias] = chatgpt[prop][nestedFunction]; // make new function, reference og one
+            }}} else { // alias direct functions
+                for (const dirAlias of subAliases) {
+                    if (dirAlias !== prop) { // don't alias og function
+                        chatgpt[dirAlias] = chatgpt[prop]; // make new function, reference og one
+            }}}
+    }}
+
+    do { // create new function per synonym per word per function
+        var newFunctionsCreated = false;
+        for (const funcName in chatgpt) {
+            if (typeof chatgpt[funcName] === 'function') {
+                const funcWords = funcName.split(/(?=[A-Zs])/); // split function name into constituent words
+                for (const funcWord of funcWords) {
+                    const synonymValues = [].concat(...synonyms // flatten into single array w/ word's synonyms
+                        .filter(arr => arr.includes(funcWord.toLowerCase())) // filter in relevant synonym sub-arrays
+                        .map(arr => arr.filter(synonym => synonym !== funcWord.toLowerCase()))); // filter out matching word
+                    for (const synonym of synonymValues) { // create function per synonym
+                        const newFuncName = camelCaser(funcWords.map(word => (word === funcWord ? synonym : word)));
+                        if (!chatgpt[newFuncName]) { // don't alias existing functions
+                            chatgpt[newFuncName] = chatgpt[funcName]; // make new function, reference og one
+                            newFunctionsCreated = true;
+    }}}}}} while (newFunctionsCreated); // loop over new functions to encompass all variations
+}
+
 // Prefix console logs w/ '🤖 chatgpt.js >> '
 const consolePrefix = '🤖 chatgpt.js >> ', ogError = console.error, ogInfo = console.info;
 console.error = (...args) => { ogError(consolePrefix + args[0], ...args.slice(1)); };
