185d76064ab291429a2e9e1ff0ac0d47a96e9df3
adamlui@protonmail.com <adamlui@protonmail.com>
2023-10-20 05:33:52 -0700

    Bumped chatgpt.js to 2.3.11

--- starters/greasemonkey/chatgpt.js-greasemonkey-starter.user.js
+++ starters/greasemonkey/chatgpt.js-greasemonkey-starter.user.js
@@ -3,12 +3,12 @@
 // @description   A Greasemonkey template to start using chatgpt.js like a boss
 // @author        chatgpt.js
 // @namespace     https://chatgpt.js.org
-// @version       2023.10.15
+// @version       2023.10.20
 // @license       MIT
 // @match         https://chat.openai.com/*
 // @icon          https://raw.githubusercontent.com/kudoai/chatgpt.js-greasemonkey-starter/main/media/images/icons/robot/icon48.png
 // @icon64        https://raw.githubusercontent.com/kudoai/chatgpt.js-greasemonkey-starter/main/media/images/icons/robot/icon64.png
-// @require       https://cdn.jsdelivr.net/gh/kudoai/chatgpt.js@8e2cdd86eb71f3b19b55928c01b33ec235d26809/dist/chatgpt-2.3.10.min.js
+// @require       https://cdn.jsdelivr.net/gh/kudoai/chatgpt.js@5d59b49dfec37e7f9c49e52441d125402a5ca10c/dist/chatgpt-2.3.11.min.js
 // @grant         GM_getValue
 // @grant         GM_setValue
 // @noframes
