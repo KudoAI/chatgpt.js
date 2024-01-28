dfbebe7b1fd71c06239db6dd301f8b3dcf3f7248
adamlui@protonmail.com <adamlui@protonmail.com>
2024-01-28 05:13:46 -0800

    Added auto-insertion of version to filename, refactored code
    + Added `unminOutputPath` + `minOutputPath`
    ± Renamed `newCJScontent` to `unminOutputContent`
    - Renamed temp file from `chatgpt.temp.js` to `chatgpt.js`
    + Added final summary log
    + Added/clarified comments
    + Added whitespace for readability

--- utils/build-minified-chatgpt.js
+++ utils/build-minified-chatgpt.js
@@ -1,16 +1,21 @@
-// Prepend [functionAliases] & [synonyms] from function-aliases.json + minify to dist/chatgpt.min.js
+// Prepend [functionAliases] & [synonyms] from data/function-aliases.json + minify to dist/chatgpt.min.js
 
+// Import libs
 const fs = require('fs'), { execSync } = require('child_process');
 
 // Prepend aliases
 const jsonContent = JSON.parse(fs.readFileSync('data/function-aliases.json', 'utf8'));
-const newCJScontent = fs.readFileSync('chatgpt.js', 'utf8').replace(
+const unminOutputContent = fs.readFileSync('chatgpt.js', 'utf8').replace(
     /^(\/\/[^\n]*\n)+/, // copyright notice
     `$&\nconst functionAliases = ${ JSON.stringify(jsonContent.functionAliases) };\n`
-      + `const synonyms = ${ JSON.stringify(jsonContent.synonyms) };\n`
+        + `const synonyms = ${ JSON.stringify(jsonContent.synonyms) };\n`
 );
-fs.writeFileSync('dist/chatgpt.temp.js', newCJScontent); // Write to temporary file
+const unminOutputPath = 'dist/chatgpt.js';
+fs.writeFileSync(unminOutputPath, unminOutputContent); // write to temp file
 
 // Minify to dist/chatgpt.min.js
-execSync('uglifyjs dist/chatgpt.temp.js -o dist/chatgpt.min.js');
-fs.unlinkSync('dist/chatgpt.temp.js');
+const version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version,
+      minOutputPath = `dist/chatgpt-${ version }.min.js`;
+execSync(`uglifyjs ${ unminOutputPath } -o ${ minOutputPath }`);
+fs.unlinkSync(unminOutputPath);
+console.info(`Success: Built ${ minOutputPath }`);
