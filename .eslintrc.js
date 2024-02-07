7688220cdfd9658a7ac9a26085ae3fec434ae7e4
adamlui@protonmail.com <adamlui@protonmail.com>
2024-02-07 00:50:15 -0800

    Removed `'**/*.md'` from `ignorePatterns`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     'extends': ['eslint:recommended'],
-    'ignorePatterns': ['**/*.md', '**/*.min.js'],
+    'ignorePatterns': ['**/*.min.js'],
     'rules': {
         'indent': 'off', 'no-unexpected-multiline': 'off', // allow whitespace anywhere
         'semi': ['error', 'always'], // enforce semicolons at end of statement
