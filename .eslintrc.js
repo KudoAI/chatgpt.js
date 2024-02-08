d54d2d7bcbc3e532b400582f4af7c3fff0bc1140
adamlui@protonmail.com <adamlui@protonmail.com>
2024-02-08 09:05:39 -0800

    Moved plugin `extends` into `overrides`
    - Removed extraneous `*` from lib override file path

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'extends': ['eslint:recommended', 'plugin:json-schema-validator/recommended', 'plugin:yml/standard'],
+    'extends': ['eslint:recommended'],
     'ignorePatterns': ['**/*.min.js', '!/.github'],
     'rules': {
         'indent': 'off', 'no-unexpected-multiline': 'off', // allow whitespace anywhere
@@ -14,6 +14,10 @@ module.exports = {
     'globals': { 'chatgpt': 'readonly', 'chrome': 'readonly', 'functionAliases': 'readonly',
                  'syncExtension': 'writable', 'synonyms': 'readonly', 'Uint8Array': 'readonly' },
     'parserOptions': { 'ecmaVersion': 2022, 'sourceType': 'script' },
-    'overrides': [{ 'files': ['**/lib*/**.js'], 'parserOptions': { 'sourceType': 'module' }}],
+    'overrides': [
+        { 'files': ['**/lib*/*.js'], 'parserOptions': { 'sourceType': 'module' }},
+        { 'files': ['**/*.json'], 'extends': ['plugin:json-schema-validator/recommended'] },
+        { 'files': ['**/*.yml'], 'extends': ['plugin:yml/standard'] }
+    ],
     'env': { 'browser': true, 'node': true, 'es6': true, 'greasemonkey': true }
 };
