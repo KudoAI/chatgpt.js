2c1a828c614b6427e82f3d323f310e69935f8397
Adam Lui 刘展鹏 <adam@kudoai.com>
2024-03-01 18:34:57 -0800

    Auto-sync test C

--- chatgpt.js
+++ chatgpt.js
@@ -1,4 +1,4 @@
-// (c) 2023–2024 KudoAI & contributors under the MIT license
+// (c) 2023–2024 KudoAI & contributors under the MIT license.
 // Source: https://github.com/kudoai/chatgpt.js
 // Latest minified release: https://cdn.jsdelivr.net/npm/@kudoai/chatgpt.js/chatgpt.min.js
 
