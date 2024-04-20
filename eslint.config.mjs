636b01e95d7e90389aae4e2e7ff21dad0b26cb4a
adamlui@protonmail.com <adamlui@protonmail.com>
2024-04-20 04:16:57 -0700

    Replaced .eslintrc.js w/ flat config file

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import eslintPluginJsonSchemaValidator from 'eslint-plugin-json-schema-validator';
+
+export default [
+    js.configs.recommended,
+    { ignores: ['**/*.min.js'] },
+    {
+        rules: {
+            'indent': 'off', 'no-unexpected-multiline': 'off', // allow whitespace anywhere
+            'semi': ['error', 'always'], // enforce semicolons at end of statement
+            'quotes': ['error', 'single'], // enforce single quotes for string literals
+            'key-spacing': ['error', { 'beforeColon': false, 'afterColon': true }], // enforce spacing in object properties
+            'comma-dangle': ['error', 'never'], // enforce no trailing commas in arrays or objects
+            'no-async-promise-executor': 'off', // allow promise executor functions to be async (to accomodate await lines)
+            'no-constant-condition': 'off', // allow constant conditions
+            'no-empty': 'off', // allow empty blocks
+            'no-inner-declarations': 'off', // allow function declarations anywhere
+            'no-useless-escape': 'off', // allow all escape chars cause ESLint sucks at detecting truly useless ones
+            'no-unused-vars': ['error', { 'caughtErrors': 'none' }] // allow unused named args in catch blocks
+        },
+        languageOptions: {
+            ecmaVersion: 2022, sourceType: 'script',
+            globals: {
+                ...globals.browser, ...globals.greasemonkey, ...globals.node,
+                chatgpt: 'readonly', chrome: 'readonly', CryptoJS: 'readonly',
+                syncExtension: 'writable'
+            }
+        }
+    },
+    { files: ['**/*.mjs', '**/lib*/*.js'], languageOptions: { sourceType: 'module' }},
+    ...eslintPluginJsonSchemaValidator.configs['flat/recommended']
+];
