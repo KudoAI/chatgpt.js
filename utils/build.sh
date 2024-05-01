c64c806ffd849842189063ae7968e8816327de80
adamlui@protonmail.com <adamlui@protonmail.com>
2024-05-01 04:31:59 -0700

    Added utils/build.sh

---
+++ utils/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+minify-js minify-js chatgpt.js dist/chatgpt.min.js --comment=" \
+© 2023–2024 KudoAI & contributors under the MIT license.\n \
+Source: https://github.com/KudoAI/chatgpt.js\n \
+User guide: https://chatgptjs.org/userguide\n \
+Latest minified release: https://cdn.jsdelivr.net/npm/@kudoai/chatgpt.js/chatgpt.min.js"
