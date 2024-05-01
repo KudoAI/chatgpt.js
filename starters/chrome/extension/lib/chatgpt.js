634758ca38ef57a4c8dd47997c8260788dcefd16
adamlui <adamlui@users.noreply.github.com>
2024-05-01 07:31:01 +0000

    Condensed DOM routine in `chatgpt.clearChats()`, added semicolons ↞ [auto-sync from `KudoAI/chatgpt.js`]

--- starters/chrome/extension/lib/chatgpt.js
+++ starters/chrome/extension/lib/chatgpt.js
@@ -426,15 +426,14 @@ const chatgpt = { // eslint-disable-line no-redeclare
 
         if (method == 'dom') {
             try { await chatgpt.getChatData(); } catch { return; } // check if chat history exists
-            chatgpt.menu.open();
-            setTimeout(() => {
+            chatgpt.menu.open(); setTimeout(() => { // open settings
                 const settingsBtn = document.querySelector(
-                    'a[role="menuitem"] svg path[d*="M11.6439 3C10.9352"]').parentNode.parentNode
-                if (settingsBtn) settingsBtn.click()
+                    'a[role="menuitem"] svg path[d*="M11.6439 3C10.9352"]').parentNode.parentNode;
+                if (settingsBtn) settingsBtn.click();
                 setTimeout(() => { // clear chats
                     const settingsBtns = document.querySelectorAll('[id*=radix] button'),
                           deleteBtn = settingsBtns[settingsBtns.length - 1];
-                    if (deleteBtn) deleteBtn.click()
+                    if (deleteBtn) deleteBtn.click();
                     setTimeout(() => { // confirm clear
                         document.querySelector('button[class*="danger"').click();
             }, 10); }, 333); }, 10);
