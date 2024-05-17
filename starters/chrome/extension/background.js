63331a06d32c30effb305f9f9bfad258d60b4bfc
Adam Lui 刘展鹏 <adam@kudoai.com>
2024-05-17 11:21:00 -0700

    Updated URL to auto-visit on install

--- starters/chrome/extension/background.js
+++ starters/chrome/extension/background.js
@@ -3,7 +3,7 @@ const allowedHosts = ['chatgpt.com', 'chat.openai.com'];
 // Add install/update actions
 chrome.runtime.onInstalled.addListener((details) => {
     chrome.storage.local.set({ 'chatgptJS_extensionDisabled': false }); // auto-enable
-    if (details.reason == 'install') chrome.tabs.create({ url: 'https://chat.openai.com/' }); // open ChatGPT
+    if (details.reason == 'install') chrome.tabs.create({ url: 'https://chatgpt.com/' }); // open ChatGPT
 });
 
 // Sync extension state/settings when ChatGPT tab active
