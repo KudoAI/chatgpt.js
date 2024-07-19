5e57ddb8c85f3d394b688a4ecef1c3363119be04
adamlui@protonmail.com <adamlui@protonmail.com>
2024-07-19 03:51:21 -0700

    Bumped chatgpt.js to 3.0.1

--- starters/greasemonkey/chatgpt.js-greasemonkey-starter.user.js
+++ starters/greasemonkey/chatgpt.js-greasemonkey-starter.user.js
@@ -3,13 +3,13 @@
 // @description   A Greasemonkey template to start using chatgpt.js like a boss
 // @author        chatgpt.js
 // @namespace     https://chatgpt.js.org
-// @version       2024.7.19
+// @version       2024.7.19.1
 // @license       MIT
 // @match         *://chatgpt.com/*
 // @match         *://chat.openai.com/*
-// @icon          https://cdn.jsdelivr.net/gh/KudoAI/chatgpt.js@3.0.0/starters/greasemonkey/media/images/icons/robot/icon48.png
-// @icon64        https://cdn.jsdelivr.net/gh/KudoAI/chatgpt.js@3.0.0/starters/greasemonkey/media/images/icons/robot/icon64.png
-// @require       https://cdn.jsdelivr.net/npm/@kudoai/chatgpt.js@3.0.0/dist/chatgpt.min.js
+// @icon          https://cdn.jsdelivr.net/gh/KudoAI/chatgpt.js@3.0.1/starters/greasemonkey/media/images/icons/robot/icon48.png
+// @icon64        https://cdn.jsdelivr.net/gh/KudoAI/chatgpt.js@3.0.1/starters/greasemonkey/media/images/icons/robot/icon64.png
+// @require       https://cdn.jsdelivr.net/npm/@kudoai/chatgpt.js@3.0.1/dist/chatgpt.min.js
 // @grant         GM_getValue
 // @grant         GM_setValue
 // @noframes
