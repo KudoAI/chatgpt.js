8ffc4a4d98929fb3f8f9399512f2d1efa7b6b114
Adam Lui 刘展鹏 <adam@kudoai.com>
2024-11-11 05:36:02 -0800

    Removed semicolons + enforcement

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import json from 'eslint-plugin-json';
+import js from '@eslint/js'
+import globals from 'globals'
+import json from 'eslint-plugin-json'
 
 export default [
     js.configs.recommended,
@@ -8,7 +8,6 @@ export default [
     {
         rules: {
             'indent': 'off', 'no-unexpected-multiline': 'off', 'key-spacing': 'off', // allow whitespace anywhere
-            'semi': ['error', 'always'], // enforce semicolons at end of statement
             'quotes': ['error', 'single', { 'allowTemplateLiterals': true }], // enforce single quotes except backticks to avoid escaping quotes
             'comma-dangle': ['error', 'never'], // enforce no trailing commas in arrays or objects
             'no-async-promise-executor': 'off', // allow promise executor functions to be async (to accomodate await lines)
@@ -29,4 +28,4 @@ export default [
     },
     { files: ['**/*.mjs', '**/lib*/*.js'], languageOptions: { sourceType: 'module' }},
     { files: ['**/*.json'], ...json.configs['recommended'] }
-];
+]
