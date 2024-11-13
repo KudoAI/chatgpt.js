09568eef1bf8eb6aa250e114213b6c8688a15f20
adamlui@protonmail.com <adamlui@protonmail.com>
2024-11-12 17:43:05 -0800

    Added `files` to JS config, moved `ignores` + `js.configs.recommended` inside for readability

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,9 +3,8 @@ import globals from 'globals'
 import json from 'eslint-plugin-json'
 
 export default [
-    js.configs.recommended,
-    { ignores: ['**/*.min.js'] },
     {
+        files: ['**/*.js', '**/*.mjs'], ignores: ['**/*.min.js'], ...js.configs.recommended,
         rules: {
             'indent': 'off', 'no-unexpected-multiline': 'off', 'key-spacing': 'off', // allow whitespace anywhere
             'quotes': ['error', 'single', { 'allowTemplateLiterals': true }], // enforce single quotes except backticks to avoid escaping quotes
