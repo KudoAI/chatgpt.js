44f8450ef2c17a7de3e0089b708096d9bc3e4cc7
Adam Lui <adam@kudoai.com>
2024-11-18 02:33:33 -0800

    Added `dom` to JS globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ export default [
             ecmaVersion: 'latest', sourceType: 'script',
             globals: {
                 ...globals.browser, ...globals.greasemonkey, ...globals.node,
-                chatgpt: 'readonly', chrome: 'readonly', CryptoJS: 'readonly'
+                chatgpt: 'readonly', chrome: 'readonly', CryptoJS: 'readonly', dom: 'readonly'
             }
         },
         plugins: { regexp },
