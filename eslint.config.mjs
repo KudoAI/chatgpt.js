c8b572bbeff4fc6aead0e6eb1addeb14be3d3990
adamlui@protonmail.com <adamlui@protonmail.com>
2024-11-24 03:04:07 -0800

    Added `js-styles/max-len` to limit lines to 120 chars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,6 +22,9 @@ export default [
             ...js.configs.recommended.rules, ...regexp.configs['flat/recommended'].rules,
             'indent': 'off', 'no-unexpected-multiline': 'off', 'key-spacing': 'off', // allow whitespace anywhere
             'js-styles/no-trailing-spaces': 'error', // ...except at ends of lines
+            'js-styles/max-len': ['error', { 'code': 120, // limit lines to 120 chars except if containing...
+                'ignoreComments': true, 'ignoreStrings': true, // ...trailing/own-line comments, quoted strings...
+                'ignoreTemplateLiterals': true, 'ignoreRegExpLiterals': true }], // ...or template/regex literals
             'quotes': ['error', 'single', { 'allowTemplateLiterals': true }], // enforce single quotes except backticks to avoid escaping quotes
             'comma-dangle': ['error', 'never'], // enforce no trailing commas in arrays or objects
             'no-async-promise-executor': 'off', // allow promise executor functions to be async (to accomodate await lines)
