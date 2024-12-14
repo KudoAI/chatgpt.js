e517eb111eab934128380fc76f1673cbe96a1be0
adamlui@protonmail.com <adamlui@protonmail.com>
2024-12-14 13:06:34 -0800

    Added server termination instruction log

--- utils/previewDocs.js
+++ utils/previewDocs.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 
 require('child_process').exec('docsify serve docs').stdout.on('data', data => {
-    console.log(data) ; require('open')('http://localhost:3000') })
+    console.log(data) ; console.log('To stop the server, press Ctrl+C')
+    require('open')('http://localhost:3000')
+})
