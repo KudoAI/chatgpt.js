4d111417b6330e0952efe6f5e9d633f4fae9e7d3
adamlui@protonmail.com <adamlui@protonmail.com>
2025-01-28 02:48:25 -0800

    Removed disabling of `no-async-promise-executor`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -32,7 +32,6 @@ export default [
             'js-styles/no-extra-semi': 'error', // disallow unnecessary semicolons
             'quotes': ['error', 'single', { 'allowTemplateLiterals': true }], // enforce single quotes except backticks to avoid escaping quotes
             'comma-dangle': ['error', 'never'], // enforce no trailing commas in arrays or objects
-            'no-async-promise-executor': 'off', // allow promise executor functions to be async (to accomodate await lines)
             'no-constant-condition': 'off', // allow constant conditions
             'no-empty': 'off', // allow empty blocks
             'no-inner-declarations': 'off', // allow function declarations anywhere
