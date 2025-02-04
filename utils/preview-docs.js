aaceb0cee977cf511565ac2201a923a6e400bf54
adamlui@protonmail.com <adamlui@protonmail.com>
2025-02-04 12:03:40 -0800

    Replaced `require('open')('http://localhost:3000')` w/ docsify-cli arg, merged logs

--- utils/preview-docs.js
+++ utils/preview-docs.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
 
-require('child_process').exec('docsify serve docs').stdout.on('data', data => {
-    console.log(data) ; console.log('To stop the server, press Ctrl+C')
-    require('open')('http://localhost:3000')
-})
+require('child_process').exec('docsify serve docs --o').stdout.on('data', data =>
+    console.log(data + 'To stop the server, press Ctrl+C'))
