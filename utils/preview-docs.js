cd31da5a2f0de29d9429235ed3dccc6e29f8700a
adamlui@protonmail.com <adamlui@protonmail.com>
2025-02-04 12:11:05 -0800

    Corrected `docsify serve` arg

--- utils/preview-docs.js
+++ utils/preview-docs.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 
-require('child_process').exec('docsify serve docs --o').stdout.on('data', data =>
+require('child_process').exec('docsify serve docs -o').stdout.on('data', data =>
     console.log(data + 'To stop the server, press Ctrl+C'))
