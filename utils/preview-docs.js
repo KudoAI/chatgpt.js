9ecf2aeec2e9aa3dace45663fba90fad463f040e
adamlui@protonmail.com <adamlui@protonmail.com>
2025-02-05 17:05:45 -0800

    Colored stop key cmd, reworded hint for readability

--- utils/preview-docs.js
+++ utils/preview-docs.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
 
+// Init UI COLORS
+const bw = '\x1b[97m', // bright white
+      nc = '\x1b[0m'   // no color
+
 require('child_process').exec('docsify serve docs -o').stdout.on('data', data =>
-    console.log(data + 'To stop the server, press Ctrl+C'))
+    console.log(data + `Press ${bw}CTRL+C${nc} to stop server`))
