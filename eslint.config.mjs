45e253a54bf5e9b6f8c4c666fbbb2220ec60c11a
Adam Lui <adam@kudoai.com>
2025-05-08 00:37:47 -0700

    Added `<app|env|modals>` to globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,9 +17,9 @@ export default [
         languageOptions: {
             ecmaVersion: 'latest', sourceType: 'script',
             globals: {
-                ...globals.browser, ...globals.greasemonkey, ...globals.node, chatgpt: 'readonly', chrome: 'readonly',
-                config: 'writable', dom: 'readonly', icons: 'writable', modals: 'writable', settings: 'writable',
-                ui: 'readonly'
+                ...globals.browser, ...globals.greasemonkey, ...globals.node, app: 'writable', chatgpt: 'readonly',
+                chrome: 'readonly', config: 'writable', dom: 'readonly', env: 'writable', icons: 'writable',
+                modals: 'writable', settings: 'writable', ui: 'readonly'
             }
         },
         plugins: { 'import': importPlugin, 'js-styles': stylisticJS, regexp },
