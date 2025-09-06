d61cb7d8b1b6d29da81a5df7b8701a501ec0b3ca
Adam Lui <adam@kudoai.com>
2025-09-05 17:39:42 -0700

    Added `feedback` to globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,8 +18,8 @@ export default [
             ecmaVersion: 'latest', sourceType: 'script',
             globals: {
                 ...globals.browser, ...globals.greasemonkey, ...globals.node, app: 'writable', chatgpt: 'readonly',
-                chrome: 'readonly', config: 'writable', dom: 'readonly', env: 'writable', icons: 'writable',
-                modals: 'writable', settings: 'writable', ui: 'readonly'
+                chrome: 'readonly', config: 'writable', dom: 'readonly', env: 'writable', feedback: 'readonly',
+                icons: 'writable', modals: 'writable', settings: 'writable', ui: 'readonly'
             }
         },
         plugins: { 'import': importPlugin, 'js-styles': stylisticJS, regexp },
