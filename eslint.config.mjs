ac0f42bb6a1ef821c01837b6a864ce289077c719
Adam Lui <adam@kudoai.com>
2026-01-18 20:19:22 -0800

    Allowed accessing named exports via default import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -40,7 +40,8 @@ export default [
             'no-empty': 'off', // allow empty blocks
             'no-inner-declarations': 'off', // allow function declarations anywhere
             'no-useless-escape': 'off', // allow all escape chars cause ESLint sucks at detecting truly useless ones
-            'no-unused-vars': ['error', { 'caughtErrors': 'none' }] // allow unused named args in catch blocks
+            'no-unused-vars': ['error', { 'caughtErrors': 'none' }], // allow unused named args in catch blocks
+            'import/no-named-as-default-member': 'off' // allow accessing named exports via default import
         }
     },
     { files: ['**/chatgpt.js'], languageOptions: { globals: { chatgpt: 'off' }}},
