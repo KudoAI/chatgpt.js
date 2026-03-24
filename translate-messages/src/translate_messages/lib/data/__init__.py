3e0ef244a49f672a0e5449fc0abfc2a628938396
Adam Lui <adam@kudoai.com>
2026-03-23 23:14:48 -0700

    Added Py deps

--- translate-messages/src/translate_messages/lib/data/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import csv
-from . import json
-from . import sns
-
-__all__ = ['csv', 'json', 'sns']
--- translate-messages/src/translate_messages/lib/data/csv.py
+++
@@ -1,3 +0,0 @@
-def parse(val: str) -> list[str]:
-    if not val : return []
-    return [item.strip() for item in val.split(',')]
