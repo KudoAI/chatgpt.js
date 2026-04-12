cd3140b545734beca0c0c82499c9a448d551054e
Adam Lui <adam@kudoai.com>
2026-04-11 19:46:49 -0700

    Deleted noisy comments

--- utils/bump.sh
+++ utils/bump.sh
@@ -26,16 +26,13 @@ case $1 in # edit subvers based on version type
 esac
 new_ver=$(printf "%s.%s.%s" "${subvers[@]}")
 
-# PULL latest changes
 echo -e "${BY}Pulling latest changes from remote to sync local repository...${NC}\n"
 git pull || (echo -e "${BR}Merge failed, please resolve conflicts!${NC}" && exit 1)
 echo ''
 
-# Bump version in package.json + package-lock.json
 echo -e "${BY}Bumping versions in package manifests...${BW}"
 npm version --no-git-tag-version "$new_ver"
 
-# Bump versions in READMEs
 echo -e "${BY}\nBumping versions in READMEs...${BW}"
 sed -i \
     -e "s/\(chatgpt\(-\|\.js@\)\)[0-9]\+\(\.[0-9]\+\)\{2\}/\1$new_ver/g" `# jsDelivr URLs` \
@@ -43,12 +40,9 @@ sed -i \
     $(find docs -regex ".*/\(README\|USERGUIDE\)\.md") ./README.md
 echo "v$new_ver"
 
-# Bump chatgpt.js version in Greasemonkey starter
 echo -e "${BY}\nBumping versions in Greasemonkey starter...${BW}\n"
 sed -i "s|\(@require.*chatgpt\.js@\)[0-9.]\+|\1$new_ver|g" starters/greasemonkey/*.user.js
 echo "chatgpt.js v$new_ver"
-
-# Bump userscript version in Greasemonkey starter
 TODAY=$(date +'%Y.%-m.%-d') # YYYY.M.D format
 if grep -q "@version\s*${TODAY}$" starters/greasemonkey/*.user.js # exact match for $TODAY
     then # bump to $TODAY.1
@@ -63,7 +57,6 @@ else # no match for $TODAY
 new_gm_ver=$(sed -n "s/.*@version\s*\(.*\)/\1/p" starters/greasemonkey/*.user.js)
 echo "chatgpt.js-greasemonkey-starter.user.js v$new_gm_ver"
 
-# Change Git author/committer to kudo-sync-bot
 echo -e "${BY}\nChanging Git author/committer to kudo-sync-bot...\n${NC}"
 if [ -n "$GPG_KEYS_PATH" ] ; then
     KEY_PATH="$GPG_KEYS_PATH/kudo-sync-bot-private-key.asc"
@@ -76,7 +69,6 @@ export GIT_AUTHOR_EMAIL="auto-sync@kudoai.com"
 export GIT_COMMITTER_NAME="kudo-sync-bot"
 export GIT_COMMITTER_EMAIL="auto-sync@kudoai.com"
 
-# Commit bumps to Git
 echo -e "${BY}\nCommitting bumps to Git...\n${NC}"
 git add package*.json
 git commit -n -m "Bumped versions in manifests to $new_ver"
@@ -85,11 +77,9 @@ git commit -n -m "Bumped versions in jsDelivr URLs to $new_ver"
 git add ./*greasemonkey-starter.user.js
 git commit -n -m "Bumped chatgpt.js to $new_ver"
 
-# Build chatgpt.min.js to dist/
 echo -e "${BY}\nBuilding chatgpt.min.js...\n${NC}"
 bash utils/build.sh
 
-# Update jsDelivr URLs for GitHub assets w/ commit hash
 echo -e "${BY}\nUpdating jsDelivr URLs for GitHub assets w/ commit hash...${NC}"
 bump_hash=$(git rev-parse HEAD)
 old_file=$(<dist/chatgpt.min.js)
@@ -100,22 +90,17 @@ if [[ "$old_file" != "$new_file" ]]
     else echo "No jsDelivr URLs for GH assets found in built files"
 fi
 
-# Commit build to Git
 echo -e "${BY}\nCommitting build to Git...\n${NC}"
 git add ./**/chatgpt.min.js
 git commit -n -m "Built chatgpt.js $new_ver"
 
-# Push to GiHub
 echo -e "${BY}\nPushing to GitHub...\n${NC}"
 git push
 
-# Publish to NPM
 if [[ "$*" == *"--publish"* ]] ; then
     echo -e "${BY}\nPublishing to npm...\n${NC}" ; npm publish ; fi
 
-# Restore OG Git config
 echo -e "${BY}\nRestoring original Git config...\n${NC}"
 while IFS='=' read -r key val ; do git config --global "$key" "$val" ; done < ~/.gitconfig.backup
 
-# Print final summary
 echo -e "\n${BG}Successfully bumped to v$new_ver$([[ "$*" == *"--publish"* ]] && echo ' and published to npm')!${NC}"
