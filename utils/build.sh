06fb3b01eec222daf7b41950bfe1cded8860280c
adamlui <adam@kudoai.com>
2026-04-29 14:15:23 -0700

    Updated build output path

--- utils/build.sh
+++ utils/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-minify-js src/chatgpt.js dist/chatgpt.min.js --comment=" \
+minify-js src/chatgpt.js ../dist/chatgpt.min.js --comment=" \
 © 2023–2026 KudoAI & contributors under the MIT license.\n \
 Source: https://github.com/KudoAI/chatgpt.js\n \
 User guide: https://chatgptjs.org/userguide\n \
