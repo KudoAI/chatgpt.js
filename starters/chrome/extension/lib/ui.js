1816c9dcd7643fcead0fb786d07fe9bb0adbd8bc
adamlui <adam@kudoai.com>
2026-04-28 21:24:44 -0700

    Deleted /starters

--- starters/chrome/extension/lib/ui.js
+++
@@ -1,6 +0,0 @@
-window.ui = {
-    getScheme() {
-        return /\b(light|dark)\b/.exec(document.documentElement.className)?.[1]
-            || ( window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light' )
-    }
-};
