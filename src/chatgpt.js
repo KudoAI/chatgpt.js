feb942b725818f020c686ac0db174d8ec8da8d0b
adamlui <adam@kudoai.com>
2026-05-26 23:49:00 -0700

    Eliminated deprecated function calls (closes KudoAI/chatgpt.js#568)

--- src/chatgpt.js
+++ src/chatgpt.js
@@ -97,7 +97,7 @@ const chatgpt = {
     // * Spaces are inserted into button labels by parsing function names in camel/kebab/snake case
 
         // Init env context
-        const scheme = chatgpt.isDarkMode() ? 'dark' : 'light',
+        const scheme = chatgpt.settings.scheme.isDark() ? 'dark' : 'light',
               isMobile = chatgpt.browser.isMobile()
 
         // Define event handlers
@@ -327,7 +327,7 @@ const chatgpt = {
         const closeSVGpath = document.createElementNS('http://www.w3.org/2000/svg', 'path')
         closeSVGpath.setAttribute('fill-rule', 'evenodd')
         closeSVGpath.setAttribute('clip-rule', 'evenodd')
-        closeSVGpath.setAttribute('fill', chatgpt.isDarkMode() ? 'white' : 'black')
+        closeSVGpath.setAttribute('fill', chatgpt.settings.scheme.isDark() ? 'white' : 'black')
         closeSVGpath.setAttribute('d', 'M13.7071 1.70711C14.0976 1.31658 14.0976 0.683417 13.7071 0.292893C13.3166 -0.0976312 12.6834 -0.0976312 12.2929 0.292893L7 5.58579L1.70711 0.292893C1.31658 -0.0976312 0.683417 -0.0976312 0.292893 0.292893C-0.0976312 0.683417 -0.0976312 1.31658 0.292893 1.70711L5.58579 7L0.292893 12.2929C-0.0976312 12.6834 -0.0976312 13.3166 0.292893 13.7071C0.683417 14.0976 1.31658 14.0976 1.70711 13.7071L7 8.41421L12.2929 13.7071C12.6834 14.0976 13.3166 14.0976 13.7071 13.7071C14.0976 13.3166 14.0976 12.6834 13.7071 12.2929L8.41421 7L13.7071 1.70711Z')
         closeSVG.append(closeSVGpath) ; closeBtn.append(closeSVG)
 
@@ -1194,7 +1194,7 @@ const chatgpt = {
     minify() { chatgpt.code.minify() },
 
     notify(...args) {
-        const scheme = chatgpt.isDarkMode() ? 'dark' : 'light'
+        const scheme = chatgpt.settings.scheme.isDark() ? 'dark' : 'light'
         let msg, position, notifDuration, shadow, toast
         if (typeof args[0] == 'object' && !Array.isArray(args[0]))
             ({ msg, position, notifDuration, shadow, toast } = args[0])
@@ -1299,7 +1299,7 @@ const chatgpt = {
 
         // Show notification
         setTimeout(() => {
-            notificationDiv.style.opacity = chatgpt.isDarkMode() ? 0.8 : 0.67 // show msg
+            notificationDiv.style.opacity = chatgpt.settings.scheme.isDark() ? 0.8 : 0.67 // show msg
             notificationDiv.style.transform = 'translateX(0)' // bring from off-screen
             notificationDiv.style.transition = 'transform 0.15s ease, opacity 0.15s ease'
         }, 10)
