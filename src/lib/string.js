b1776b938fa50c980aef666eb358803ae31302d0
adamlui <adam@kudoai.com>
2026-06-09 09:06:02 -0700

    Tightened `string.looksLikePath()` matching (fixes KudoAI/chatgpt.js#689)

--- src/lib/string.js
+++ src/lib/string.js
@@ -1,10 +1,13 @@
+const path = require('path')
+
 module.exports = {
 
     toTitleCase(str) { return str.toLowerCase().replace(/\b\w/g, char => char.toUpperCase()) },
 
     looksLikePath(str) {
-        const path = require('path')
-        return path.isAbsolute(str) || str.includes(path.sep) || /^\.\.?[\\/]/.test(str) || path.extname(str)
+        if (typeof str != 'string') return false
+        if (path.isAbsolute(str) || /^\.\.?[\\/]/.test(str)) return true
+        return false
     },
 
     looksLikeURL(str) {
