e8b332081f8747023b6e452ebf68e1cf91b865e2
adamlui <adam@kudoai.com>
2026-06-09 09:39:02 -0700

    Condensed `looksLikePath()`, alphabetized functions

--- src/lib/string.js
+++ src/lib/string.js
@@ -2,17 +2,13 @@ const path = require('path')
 
 module.exports = {
 
-    toTitleCase(str) { return str.toLowerCase().replace(/\b\w/g, char => char.toUpperCase()) },
-
-    looksLikePath(str) {
-        if (typeof str != 'string') return false
-        if (path.isAbsolute(str) || /^\.\.?[\\/]/.test(str)) return true
-        return false
-    },
+    looksLikePath(str) { return typeof str == 'string' && (path.isAbsolute(str) || /^\.\.?[\\/]/.test(str)) },
 
     looksLikeURL(str) {
         if (!str || typeof str != 'string') return false
         try { return /^https?:$/.test(new URL(str).protocol) }
         catch (err) { return false }
-    }
+    },
+
+    toTitleCase(str) { return str.toLowerCase().replace(/\b\w/g, char => char.toUpperCase()) }
 }
