eaa4e2466866201300bb6556e4ac835659dc365a
adamlui <adam@kudoai.com>
2026-06-10 07:15:48 -0700

    Eliminated `deps` arg from `resolveSrc()`

--- src/cli/lib/resolver.js
+++ src/cli/lib/resolver.js
@@ -1,17 +1,11 @@
 module.exports = {
-    async resolveSrc(src, deps = {}) {
-        const {
-              fetch = require('./data').fetch,
-              fs = require('fs'),
-              string = require('../../lib/string')
-        } = deps
-
+    async resolveSrc(src) {
         if (src && typeof src == 'object' && src.type == 'url') {
-            const text = await (await fetch(src.value)).text()
+            const text = await (await require('./data').fetch(src.value)).text()
             if (!text) throw new Error(`Empty response from ${src.value}`)
             return text
-        } else if (typeof src == 'string' && string.looksLikePath(src))
-            return fs.readFileSync(src, 'utf8')
+        } else if (typeof src == 'string' && require('../../lib/string').looksLikePath(src))
+            return require('fs').readFileSync(src, 'utf8')
         else return src
     }
 }
