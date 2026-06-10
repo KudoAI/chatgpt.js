fab22f5edb612aad702d65d70177ce39f363b508
adamlui <adam@kudoai.com>
2026-06-10 07:20:07 -0700

    Added arg validation to `resolveSrc()`, condensed conditions

--- src/cli/lib/resolver.js
+++ src/cli/lib/resolver.js
@@ -1,10 +1,11 @@
 module.exports = {
     async resolveSrc(src) {
-        if (src && typeof src == 'object' && src.type == 'url') {
+        if (!src) return log.error(`'src' arg required by resolveSrc()`)
+        if (src.type == 'url') {
             const text = await (await require('./data').fetch(src.value)).text()
             if (!text) throw new Error(`Empty response from ${src.value}`)
             return text
-        } else if (typeof src == 'string' && require('../../lib/string').looksLikePath(src))
+        } else if (require('../../lib/string').looksLikePath(src))
             return require('fs').readFileSync(src, 'utf8')
         else return src
     }
