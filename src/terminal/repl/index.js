7512f850967f1c62f5dedf7423899908cd0f09ae
Adam Lui <adam@kudoai.com>
2026-06-12 16:11:32 -0700

    Merged lines, rid never-called `log.debug`

--- src/terminal/repl/index.js
+++ src/terminal/repl/index.js
@@ -13,12 +13,8 @@ async function start() {
     log.info(`${cli.msgs.info_startingInteractiveMode}. ${
         cli.msgs.info_type} '/help' ${cli.msgs.info_forCmds}, '/exit' ${cli.msgs.info_toQuit}.`)
     const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        prompt: `${log.colors.bg}>>> ${log.colors.nc}`
-    })
+        input: process.stdin, output: process.stdout, prompt: `${log.colors.bg}>>> ${log.colors.nc}` })
     cli.msgChain = messages.loadChain()
-    log.debug(`Loaded ${cli.msgChain.length} messages from chain.`)
     rl.prompt()
     for await (const rawInput of rl) {
         const input = rawInput.trim()
