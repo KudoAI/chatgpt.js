ef60ad974a0df6db3a65d0850faccf07816450c9
Adam Lui <adam@kudoai.com>
2026-06-15 00:59:41 -0700

    Prepended `npx` to `minify-js` cmd to test fix https://github.com/KudoAI/chatgpt.js/actions/runs/27529028757/job/81369354570

--- utils/build.sh
+++ utils/build.sh
@@ -6,7 +6,7 @@ cp assets/data/_locales/en/messages.json dist/data/messages.json
 cp package-data.json dist/data/package-data.json
 cp -r src/terminal dist/
 
-minify-js src/chatgpt.js ../dist/chatgpt.min.js --comment=" \
+npx minify-js src/chatgpt.js ../dist/chatgpt.min.js --comment=" \
 © 2023–2026 KudoAI & contributors under the MIT license.\n \
 Source: https://github.com/KudoAI/chatgpt.js\n \
 User guide: https://github.com/KudoAI/chatgpt.js/blob/main/docs/USERGUIDE.md\n \
