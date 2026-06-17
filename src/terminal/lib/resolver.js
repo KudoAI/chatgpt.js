e3b07470998b9ef308bd3b84dcede0d3fead85bf
Adam Lui <adam@kudoai.com>
2026-06-17 14:25:15 -0700

    Deleted test logs

--- src/terminal/lib/resolver.js
+++ src/terminal/lib/resolver.js
@@ -4,16 +4,12 @@ module.exports = {
     async resolveSrc(src) {
         if (!src || typeof src != 'string') throw new Error(`'src' string arg required by resolveSrc()`)
         if (string.looksLikeURL(src)) {
-            console.log('looks like url')
             const text = await (await require('./data').fetch(src)).text()
             if (!text) throw new Error(`Empty response from ${src}`)
             return { text, type: 'url' }
-        } else if (string.looksLikePath(src)) {
-            console.log('looks like path')
+        } else if (string.looksLikePath(src))
             return { text: require('fs').readFileSync(src, 'utf8'), type: 'path' }
-        } else {
-            console.log('looks like inline')
+        else
             return { text: src, type: 'inline' }
-        }
     }
 }
